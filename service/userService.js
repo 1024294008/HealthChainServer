8e7ed7c4a498a8ac0238437fd578464c45361a96
hupeng <13515616243@163.com>
2020-03-31 15:48:12 +0800

    fix bug

--- service/userService.js
+++ service/userService.js
@@ -12,7 +12,7 @@ function login(req, callback){
   if(req.body && req.body.account && req.body.password){
     dao.userDao.findByAccount(req.body.account, function(status, result){
       // 验证登录
-      if(1 === status && result[0].password === req.body.password){
+      if(1 === status && result[0] && result[0].password === req.body.password){
         obj._code = '200'
         obj._msg = '登录成功'
         obj._data.token = createToken({id: result[0].id, type: 'user'})
