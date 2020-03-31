51513e629f82ed8bebe853e5af054efc0f1988dc
bilongge <760557109@qq.com>
2020-03-31 14:36:48 +0800

    dao

--- dao/index.js
+++ dao/index.js
@@ -3,17 +3,17 @@
 var adminDao = require('./adminDao')
 var logDao = require('./logDao')
 var user_healthdataDao = require('./user_healthdataDao')
-<<<<<<< HEAD
 var transactionrecordDao = require('./transactionrecordDao')
-=======
 var userDao= require('./userDao')
->>>>>>> hupeng
+var medicalServiceDao = require('./medicalServiceDao')
+// var orgDao = require('./orgDao')
+// var visitorrecordDao = require('./visitorrecordDao')
 
 exports.adminDao = adminDao
 exports.logDao = logDao
 exports.user_healthdataDao = user_healthdataDao
-<<<<<<< HEAD
 exports.transactionrecordDao = transactionrecordDao
-=======
 exports.userDao = userDao
->>>>>>> hupeng
+exports.medicalServiceDao = medicalServiceDao
+// exports.orgDao = orgDao
+// exports.visitorrecordDao = visitorrecordDao
