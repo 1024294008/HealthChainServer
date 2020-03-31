e45ce3f94c9954123ba93b7495c0280b584b288e
hupeng <13515616243@163.com>
2020-03-31 10:01:52 +0800

    userDao

--- dao/index.js
+++ dao/index.js
@@ -1,7 +1,9 @@
 var adminDao = require('./adminDao')
 var logDao = require('./logDao')
 var user_healthdataDao = require('./user_healthdataDao')
+var userDao= require('./userDao')
 
 exports.adminDao = adminDao
 exports.logDao = logDao
 exports.user_healthdataDao = user_healthdataDao
+exports.userDao = userDao
