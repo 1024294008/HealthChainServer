026ebd92915761bd1cf8f7e6439e5c62b424a1fe
bilongge <760557109@qq.com>
2020-04-02 18:20:24 +0800

    智能合约

--- contracts/Org_UserList.sol
+++ contracts/Org_UserList.sol
@@ -1,42 +1,38 @@
 pragma solidity >=0.4.21 <0.7.0;
 
+// 声明其他合约的接口  -- 授权合约
+interface AdminContract{
+    function getAdminAddress()  external view returns(address);
+}
+
 contract Org_UserList{
 
   // 机构和用户的映射
   mapping(address => address[]) userList;
 
-  // 管理员地址
-  address adminAddr;
+  // 其他合约地址
+  address adminContractAddr;
 
-  constructor() public{
-    adminAddr = msg.sender;
-  }
+  // 其他合约实例
+  AdminContract adminContract;
 
-  // 查看管理员地址
-  function getAdminAddr() public view returns(address){
-    return adminAddr;
-  }
-
-  // 修改管理员
-  function setAdminAddr(address _addr) public {
-
-    assert(msg.sender == adminAddr);
-    adminAddr = _addr;
+  // 构造函数 -- 指定调用合约的地址
+  constructor(address _adminContractAddr) public{
+    adminContractAddr = _adminContractAddr;
+    adminContract = AdminContract(adminContractAddr);   // 通过地址拿到合约实例   --  将地址强制转换为合约
   }
 
   // 获取机构对应的用户列表
   function getUserList(address orgAddr) public view returns(address[] memory) {
 
-    assert(msg.sender == adminAddr);
     return userList[orgAddr];
   }
 
   // 为机构添加用户
   function addUser(address orgAddr, address userAddr) public {
 
-    assert(msg.sender == adminAddr);
+    assert(msg.sender == adminContract.getAdminAddress());
     userList[orgAddr].push(userAddr);
   }
-
 
 }
