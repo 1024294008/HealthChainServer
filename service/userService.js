da6ac002bd02bce76fcbb8fa0808391cbeaaf2f1
hupeng <13515616243@163.com>
2020-04-04 18:25:50 +0800

    userservice

--- service/userService.js
+++ service/userService.js
@@ -159,28 +159,152 @@ function getMedicalServiceList(req, callback){
   })
 }
 
-// 获取健康数据
+// 获取用户自己的某一条健康数据
 function getHealthData(req, callback){
   if(req.body && req.body.verify){
-    dao.user_healthdataDao.findAllDataById(req.body.verify.id, function(status, result){
-      if(1 === status){
-        obj._code = '200'
-        obj._msg = '查询成功'
-        obj._data = {
-          healthMetaDataList: result
-        }
-        callback(obj)
+    dao.userDao.findByPrimaryKey(req.verify.id, function(status, user){
+      // 显示最近一条数据
+      if(1 === status && -1 === req.body.index){
+        dao.ethDao.getHealthCount(user.ethAddress, function(status, count){
+          if(1 === status && count !== 0){
+            dao.ethDao.getHDataByIndex(count - 1, user.ethAddress, user.contractAddr, function(status, result){
+              if(1 === status){
+                obj._code = '200'
+                obj._msg = '查询成功'
+                obj._data = result
+                callback(obj)
+              } else {
+                obj._code = '201'
+                obj._msg = '查询失败'
+                obj._data = {}
+                callback(obj)
+              }
+            })
+          } else if(1 === status && 0 === count){
+            obj._code = '202'
+            obj._msg = '无数据'
+            obj._data = {}
+            callback(obj)
+          } else {
+            obj._code = '201'
+            obj._msg = '查询失败'
+            obj._data = {}
+            callback(obj)
+          }
+        })
+      } else if(1 === status && req.body.index) {  // 显示某一条数据
+        dao.ethDao.getHDataByIndex(req.body.index, user.ethAddress, user.contractAddr, function(status, result){
+          if(1 === status){
+            obj._code = '200'
+            obj._msg = '查询成功'
+            obj._data = result
+            callback(obj)
+          } else {
+            obj._code = '201'
+            obj._msg = '查询失败'
+            obj._data = {}
+            callback(obj)
+          }
+        })
       } else {
         obj._code = '201'
         obj._msg = '查询失败'
         obj._data = {}
+        callback(obj)
       }
     })
   } else {
-    obj._code = '200'
+    obj._code = '201'
     obj._msg = '查询失败'
     obj._data = {}
+    callback(obj)
   }
+}
+
+// 获取用户自己的全部健康数据
+function getHealthDataList(req, callback){
+
+}
+
+// 获取用户自己的健康数据条数
+function getHealthCount(req, callback){
+  if(req.body && req.body.verify && req.body.verify.id){
+    dao.userDao.findByPrimaryKey(req.body.verify.id, function(status, user){
+      if(1 === status){
+        dao.ethDao.getHealthCount(user.ethAddress, function(status, result){
+          if(1 === status){
+            obj._code = '200'
+            obj._msg = '查询成功'
+            obj._data = {
+              count: result
+            }
+            callback(obj)
+          } else {
+            obj._code = '201'
+            obj._msg = '查询失败'
+            obj._data = {}
+            callback(obj)
+          }
+        })
+      }
+    })
+  } else{
+    obj._code = '201'
+    obj._msg = '查询失败'
+    obj._data = {}
+    callback(obj)
+  }
+}
+
+// 用户转账
+function transfer(req, callback){
+  if(req.body && req.body.verify && req.body.verify.id){
+
+  }
+}
+
+function getBalance(req, callback){
+  if(req.body && req.body.verify && req.body.verify.id){
+    dao.userDao.findByPrimaryKey(req.body.verify.id, function(status, user){
+      if(1 === status){
+        dao.ethDao.getBalance(user.ethAddress, function(status, result){
+          if(1 === status){
+            obj._code = '200'
+            obj._msg = '查询成功'
+            obj._data = {
+              balance: result
+            }
+            callback(obj)
+          } else {
+            obj._code = '201'
+            obj._msg = '查询失败'
+            obj._data = {}
+            callback(obj)
+          }
+        })
+      }
+    })
+  } else{
+    obj._code = '201'
+    obj._msg = '查询失败'
+    obj._data = {}
+    callback(obj)
+  }
+}
+
+function getBlockInfo(req, callback){
+  if(req.body && req.body.verify && req.body.verify.id){
+    // dao.adminEthDao.
+  } else{
+    obj._code = '201'
+    obj._msg = '查询失败'
+    obj._data = {}
+    callback(obj)
+  }
+}
+
+function getMinerInfo(req, callback){
+
 }
 
 module.exports = {
