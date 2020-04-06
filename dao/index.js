4bf148e73256d05bca8c52130b82a7334a1541f9
xieliangliang <1829394180@qq.com>
2020-04-06 10:57:28 +0800

    冲突解决

---
+++ dao/index.js
@@ -0,0 +1,28 @@
+/* dao入口文件 */
+
+var adminDao = require('./adminDao')
+var logDao = require('./logDao')
+var user_healthdataDao = require('./user_healthdataDao')
+var transactionrecordDao = require('./transactionrecordDao')
+var userDao= require('./userDao')
+var medicalServiceDao = require('./medicalServiceDao')
+var orgDao = require('./orgDao')
+var ethDao = require('./ethDao')
+var adminEthDao = require('./adminEthDao')
+// var visitorrecordDao = require('./visitorrecordDao')
+var ethDao = require('./ethDao')
+var adminEthDao = require('./adminEthDao')
+
+
+exports.adminDao = adminDao
+exports.logDao = logDao
+exports.user_healthdataDao = user_healthdataDao
+exports.transactionrecordDao = transactionrecordDao
+exports.userDao = userDao
+exports.medicalServiceDao = medicalServiceDao
+exports.orgDao = orgDao
+exports.ethDao = ethDao
+exports.adminEthDao = adminEthDao
+// exports.visitorrecordDao = visitorrecordDao
+exports.ethDao = ethDao
+exports.adminEthDao = adminEthDao
