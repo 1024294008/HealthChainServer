62cc467bb0fabc4e82b00ff7126d8b5888ddbc0b
hupeng <13515616243@163.com>
2020-04-08 18:28:11 +0800

    sort

--- routes/users/distanceChart.js
+++ routes/users/distanceChart.js
@@ -6,7 +6,6 @@ var checkToken = require('../../middleware/checkToken')
 
 router.post('/', checkToken, function(req, res, next){
   service.userService.getHealthDataList(req, function(result){
-    console.log(result)
     res.render('distanceChart', {healthDataList: result._data});
   })
 })
