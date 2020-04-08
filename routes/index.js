6d03dfa80babf14aa6d8c51314f6b4581a4425a7
yangyang wu <1614468289@qq.com>
2020-04-08 16:52:08 +0800

    merge

--- routes/index.js
+++ routes/index.js
@@ -20,14 +20,11 @@ module.exports = function(app){
   app.use('/api/user/findBytransactRemarks', require('./users/findBytransactRemarks'))
   app.use('/api/user/findRecordByEthAddress', require('./users/findRecordByEthAddress'))
   app.use('/api/user/UserTransactionRecordDetail', require('./users/UserTransactionRecordDetail'))
-<<<<<<< HEAD
   app.use('/api/user/distanceChart', require('./users/distanceChart'))
   app.use('/api/user/uploadTimeChart', require('./users/uploadTimeChart'))
 
   app.use('/api/user/permitVisitChart', require('./users/permitVisitChart'))
-=======
   app.use('/api/user/sleepQualityChart', require('./users/sleepQualityChart'))
->>>>>>> 356c6b25973c511dd05d516367afc912cd6a6463
 
 
 
