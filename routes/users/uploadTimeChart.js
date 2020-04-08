4151b70c8c22c8741a61d5e5b80aae52a3f85b7d
hupeng <13515616243@163.com>
2020-04-08 11:58:59 +0800

    uploadtime chart

--- routes/users/uploadTimeChart.js
+++ routes/users/uploadTimeChart.js
@@ -7,7 +7,7 @@ var checkToken = require('../../middleware/checkToken')
 router.post('/', checkToken, function(req, res, next){
   service.userService.getHealthDataList(req, function(result){
     console.log(result)
-    res.render('distanceChart', {healthDataList: result._data});
+    res.render('uploadTimeChart', {healthDataList: result._data});
   })
 })
 
